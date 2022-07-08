00dc80fcf6a46d7dec56503f680a734898f94c90
Konstantin Gromov <Gromov.Konst95@gmail.com>
2022-07-08 16:11:41 +0300

    added triangleTests

---
+++ AreaCalculatorTest/TriangleTests.cs
@@ -0,0 +1,30 @@
+﻿using AreaCalculator;
+using NUnit.Framework;
+
+namespace AreaCalculatorTest
+{
+    [TestFixture]
+    public class TriangleTests
+    {
+        [Test]
+        [TestCase(5, 12, 13, 30)]
+        [TestCase(1, 1, 1, 0.433)]
+        [TestCase(4, 5, 6, 9.92)]
+        public void TriangleArea_ShouldWork(double a,double b,double c, double expected)
+        {
+            Triangle triangle= new Triangle(a,b,c);
+
+            double actual = triangle.Area;
+
+            Assert.AreEqual(expected, actual, 0.01);
+        }
+
+        [Test]
+        [TestCase(0,0,0)]
+        [TestCase(-1,-1,-1)]
+        public void Triangle_ShouldFail(double a,double b,double c)
+        {
+            Assert.Throws<ArgumentException>(() => new Triangle(a, b, c));
+        }
+    }
+}
