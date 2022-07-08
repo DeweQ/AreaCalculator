b09b3e605cd50aa519478c120598be2ab8b8b62c
Konstantin Gromov <Gromov.Konst95@gmail.com>
2022-07-08 14:06:54 +0300

    Add project files.

---
+++ AreaCalculator/Circle.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AreaCalculator
+{
+    public class Circle : IShape
+    {
+        public double Radius { get; init; }
+        public Circle(double radius)
+        {
+            Radius = radius;
+        }
+
+        public double Area
+        {
+            get => Math.PI * Math.Pow(Radius, 2);
+        }
+    }
+}
