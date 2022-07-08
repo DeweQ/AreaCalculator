b8298e4611608a9acd7b85504e67d7686b7bc1e8
Konstantin Gromov <Gromov.Konst95@gmail.com>
2022-07-08 16:42:50 +0300

    added check for right triangle and area calculation for it

--- AreaCalculator/Triangle.cs
+++ AreaCalculator/Triangle.cs
@@ -26,12 +26,55 @@ namespace AreaCalculator
         {
             get
             {
-                 return Math.Sqrt(SemiPerimeter * (SemiPerimeter - A) * (SemiPerimeter - B) * (SemiPerimeter - C));
+
+                if (IsRight()) return GetAreaOfRightTriangle();
+                return GetAreaOfAnyTriangle();
+
             }
+        }
+
+        private double GetAreaOfAnyTriangle()
+        {
+            double sp = SemiPerimeter;
+            double result = Math.Sqrt(sp * (sp - A) * (sp - B) * (sp - C));
+            return result;
+        }
+
+        // much more calculations than three sides formula. probably better without it
+        private double GetAreaOfRightTriangle()
+        {
+            double result = 0;
+            switch (FindHypotenuse())
+            {
+                case 'A':
+                    result = B * C / 2;
+                    break;
+                case 'B':
+                    result = A * C / 2;
+                    break;
+                case 'C':
+                    result = A * B / 2;
+                    break;
+            }
+            return result;
+        }
+
+        private char FindHypotenuse()
+        {
+            var A2 = Math.Pow(A, 2);
+            var B2 = Math.Pow(B, 2);
+            var C2 = Math.Pow(C, 2);
+            if (A2 * B2 == C2)
+                return 'C';
+            if (A2 * C2 == B2)
+                return 'B';
+            return 'A';
         }
 
         private bool IsRight()
         {
+            if (A == B && A == C)
+                return false;
             var A2 = Math.Pow(A, 2);
             var B2 = Math.Pow(B, 2);
             var C2 = Math.Pow(C, 2);
