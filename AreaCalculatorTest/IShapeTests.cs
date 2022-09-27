a69cba10cdcfea11d522c983059dfcc87281684c
Konstantin Gromov <Gromov.Konst95@gmail.com>
2022-09-27 14:57:06 +0300

    Added tests

---
+++ AreaCalculatorTest/IShapeTests.cs
@@ -0,0 +1,43 @@
+﻿using AreaCalculator;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AreaCalculatorTest
+{
+    [TestFixture]
+    public class IShapeTests
+    {
+        [Test]
+        public void IShapeTester()
+        {
+            List<(IShape Shape,double Expected)> tesetCases = new List<(IShape, double)> 
+            { 
+                (new Circle(5),78.54),
+                (new Circle(8),201.06),
+                (new Circle(14),615.8),
+                (new RightTriangle(12,7.2,9.6),34.56),
+                (new RightTriangle(17,5.8,15.98),46.342),
+                (new RightTriangle(21,9.9,18.52),91.674),
+                (new Triangle(4,5,6),9.922),
+                (new Triangle(15,6,13),38.68),
+                (new Triangle(21,9.9,18.52),91.674),
+            };
+
+            foreach(var testCase in tesetCases)
+            {
+                IShapeGetArea_ShouldWork(testCase.Shape,testCase.Expected);
+            }
+
+        }
+        private void IShapeGetArea_ShouldWork(IShape shape, double expected)
+        {
+            double actual = shape.GetArea();
+
+            Assert.AreEqual(expected, actual,0.1);
+        }
+    }
+}
---
+++ AreaCalculatorTest/RightTriangleTests.cs
@@ -0,0 +1,27 @@
+﻿using AreaCalculator;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AreaCalculatorTest
+{
+    [TestFixture]
+    public class RightTriangleTests
+    {
+        [Test]
+        [TestCase(13,5,12,30)]
+        [TestCase(26,10,24,120)]
+        [TestCase(15,9,12,54)]
+        public void RightTriangleArea_ShouldWork(double hypotenuse, double a, double b, double expected)
+        {
+            RightTriangle triangle = new RightTriangle(hypotenuse, a, b);
+
+            double actual = triangle.GetArea();
+
+            Assert.AreEqual(expected, actual);
+        }
+    }
+}
